37c764a10460bdd64d900915fe8526fa6dc11bf8
Trần Minh Phúc <50334644+TranMinhPhuc220420@users.noreply.github.com>
2019-11-20 07:57:25 +0700

    Form MDI main vs Login

---
+++ Project/frmLogin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if(txtUsername.Text == "admin" && txtPassword.Text == "123")
+            {
+                MessageBox.Show("OKe");
+            }
+            else
+            {
+                MessageBox.Show("Not oke");
+                txtPassword.Text = "";
+            }
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+            if(txtUsername.Text == string.Empty)
+            {
+                erpTxt.SetError(txtUsername, "Chưa nhập dữ liệu");
+            }
+            else
+            {
+                erpTxt.Clear();
+            }
+        }
+    }
+}
