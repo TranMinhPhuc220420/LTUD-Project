45eb6c2e6af6cd54e1dd57a30ec4fab55b1f8e35
dangtuanphong238 <dangtuanphong238@github.com>
2019-11-27 14:12:12 +0700

    update sql insert
    file SQL_QLNS.sql insert xong

--- Project/frmChucVu.Designer.cs
+++ Project/frmChucVu.Designer.cs
@@ -30,24 +30,24 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmChucVu));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.txtMaCVTim = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtTenCV = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMaCV = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvChucVu = new System.Windows.Forms.DataGridView();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.btnExit = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
             this.btnUpdate = new System.Windows.Forms.Button();
             this.btnSearch = new System.Windows.Forms.Button();
             this.btnAdd = new System.Windows.Forms.Button();
-            this.dgvChucVu = new System.Windows.Forms.DataGridView();
-            this.txtMaCV = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtTenCV = new System.Windows.Forms.TextBox();
-            this.txtMaCVTim = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
-            this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvChucVu)).BeginInit();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -57,9 +57,35 @@
             this.panel1.Controls.Add(this.txtMaCVTim);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1056, 100);
+            this.panel1.Size = new System.Drawing.Size(792, 81);
             this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 30F);
+            this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Location = new System.Drawing.Point(326, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 54);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Chức vụ";
+            // 
+            // txtMaCVTim
+            // 
+            this.txtMaCVTim.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.txtMaCVTim.Enabled = false;
+            this.txtMaCVTim.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.txtMaCVTim.Location = new System.Drawing.Point(554, 50);
+            this.txtMaCVTim.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMaCVTim.Name = "txtMaCVTim";
+            this.txtMaCVTim.Size = new System.Drawing.Size(236, 27);
+            this.txtMaCVTim.TabIndex = 8;
+            this.txtMaCVTim.Text = "nhập mã chức vụ tìm...";
             // 
             // panel2
             // 
@@ -70,10 +96,70 @@
             this.panel2.Controls.Add(this.dgvChucVu);
             this.panel2.Controls.Add(this.panel3);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 100);
+            this.panel2.Location = new System.Drawing.Point(0, 81);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1056, 425);
+            this.panel2.Size = new System.Drawing.Size(792, 346);
             this.panel2.TabIndex = 1;
+            // 
+            // txtTenCV
+            // 
+            this.txtTenCV.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtTenCV.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.txtTenCV.Location = new System.Drawing.Point(131, 88);
+            this.txtTenCV.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTenCV.Name = "txtTenCV";
+            this.txtTenCV.Size = new System.Drawing.Size(236, 27);
+            this.txtTenCV.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.label3.Location = new System.Drawing.Point(26, 90);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tên chức vụ:";
+            // 
+            // txtMaCV
+            // 
+            this.txtMaCV.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtMaCV.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.txtMaCV.Location = new System.Drawing.Point(131, 46);
+            this.txtMaCV.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMaCV.Name = "txtMaCV";
+            this.txtMaCV.Size = new System.Drawing.Size(236, 27);
+            this.txtMaCV.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
+            this.label2.Location = new System.Drawing.Point(26, 48);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mã chức vụ: ";
+            // 
+            // dgvChucVu
+            // 
+            this.dgvChucVu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvChucVu.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgvChucVu.Location = new System.Drawing.Point(0, 163);
+            this.dgvChucVu.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvChucVu.Name = "dgvChucVu";
+            this.dgvChucVu.RowTemplate.Height = 24;
+            this.dgvChucVu.Size = new System.Drawing.Size(624, 183);
+            this.dgvChucVu.TabIndex = 1;
             // 
             // panel3
             // 
@@ -84,22 +170,11 @@
             this.panel3.Controls.Add(this.btnSearch);
             this.panel3.Controls.Add(this.btnAdd);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel3.Location = new System.Drawing.Point(832, 0);
+            this.panel3.Location = new System.Drawing.Point(624, 0);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(224, 425);
+            this.panel3.Size = new System.Drawing.Size(168, 346);
             this.panel3.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 30F);
-            this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.label1.Location = new System.Drawing.Point(434, 19);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(226, 67);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Chức vụ";
             // 
             // btnExit
             // 
@@ -109,10 +184,11 @@
             this.btnExit.Font = new System.Drawing.Font("Cambria", 11.8F);
             this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
             this.btnExit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnExit.Location = new System.Drawing.Point(8, 325);
+            this.btnExit.Location = new System.Drawing.Point(6, 265);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.btnExit.Size = new System.Drawing.Size(209, 68);
+            this.btnExit.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.btnExit.Size = new System.Drawing.Size(157, 55);
             this.btnExit.TabIndex = 7;
             this.btnExit.Text = "Th&oát";
             this.btnExit.UseVisualStyleBackColor = true;
@@ -125,10 +201,11 @@
             this.btnDelete.Font = new System.Drawing.Font("Cambria", 11.8F);
             this.btnDelete.Image = ((System.Drawing.Image)(resources.GetObject("btnDelete.Image")));
             this.btnDelete.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnDelete.Location = new System.Drawing.Point(8, 165);
+            this.btnDelete.Location = new System.Drawing.Point(6, 135);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.btnDelete.Size = new System.Drawing.Size(209, 68);
+            this.btnDelete.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.btnDelete.Size = new System.Drawing.Size(157, 55);
             this.btnDelete.TabIndex = 5;
             this.btnDelete.Text = "&Xoá";
             this.btnDelete.UseVisualStyleBackColor = true;
@@ -141,10 +218,11 @@
             this.btnUpdate.Font = new System.Drawing.Font("Cambria", 11.8F);
             this.btnUpdate.Image = ((System.Drawing.Image)(resources.GetObject("btnUpdate.Image")));
             this.btnUpdate.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnUpdate.Location = new System.Drawing.Point(8, 245);
+            this.btnUpdate.Location = new System.Drawing.Point(6, 200);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(2);
             this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.btnUpdate.Size = new System.Drawing.Size(209, 68);
+            this.btnUpdate.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.btnUpdate.Size = new System.Drawing.Size(157, 55);
             this.btnUpdate.TabIndex = 6;
             this.btnUpdate.Text = "&Sửa";
             this.btnUpdate.UseVisualStyleBackColor = true;
@@ -157,10 +235,11 @@
             this.btnSearch.Font = new System.Drawing.Font("Cambria", 11.8F);
             this.btnSearch.Image = ((System.Drawing.Image)(resources.GetObject("btnSearch.Image")));
             this.btnSearch.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnSearch.Location = new System.Drawing.Point(8, 5);
+            this.btnSearch.Location = new System.Drawing.Point(6, 5);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(2);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.btnSearch.Size = new System.Drawing.Size(209, 68);
+            this.btnSearch.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.btnSearch.Size = new System.Drawing.Size(157, 55);
             this.btnSearch.TabIndex = 3;
             this.btnSearch.Text = "&Tìm";
             this.btnSearch.UseVisualStyleBackColor = true;
@@ -173,88 +252,25 @@
             this.btnAdd.Font = new System.Drawing.Font("Cambria", 11.8F);
             this.btnAdd.Image = ((System.Drawing.Image)(resources.GetObject("btnAdd.Image")));
             this.btnAdd.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnAdd.Location = new System.Drawing.Point(8, 85);
+            this.btnAdd.Location = new System.Drawing.Point(6, 70);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.btnAdd.Size = new System.Drawing.Size(209, 68);
+            this.btnAdd.Padding = new System.Windows.Forms.Padding(15, 0, 0, 0);
+            this.btnAdd.Size = new System.Drawing.Size(157, 55);
             this.btnAdd.TabIndex = 4;
             this.btnAdd.Text = "T&hêm";
             this.btnAdd.UseVisualStyleBackColor = true;
             // 
-            // dgvChucVu
-            // 
-            this.dgvChucVu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvChucVu.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.dgvChucVu.Location = new System.Drawing.Point(0, 200);
-            this.dgvChucVu.Name = "dgvChucVu";
-            this.dgvChucVu.RowTemplate.Height = 24;
-            this.dgvChucVu.Size = new System.Drawing.Size(832, 225);
-            this.dgvChucVu.TabIndex = 1;
-            // 
-            // txtMaCV
-            // 
-            this.txtMaCV.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.txtMaCV.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
-            this.txtMaCV.Location = new System.Drawing.Point(175, 56);
-            this.txtMaCV.Name = "txtMaCV";
-            this.txtMaCV.Size = new System.Drawing.Size(314, 32);
-            this.txtMaCV.TabIndex = 1;
-            // 
-            // label2
-            // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
-            this.label2.Location = new System.Drawing.Point(34, 59);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(135, 26);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Mã chức vụ: ";
-            // 
-            // label3
-            // 
-            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
-            this.label3.Location = new System.Drawing.Point(34, 111);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(135, 26);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Tên chức vụ:";
-            // 
-            // txtTenCV
-            // 
-            this.txtTenCV.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.txtTenCV.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
-            this.txtTenCV.Location = new System.Drawing.Point(175, 108);
-            this.txtTenCV.Name = "txtTenCV";
-            this.txtTenCV.Size = new System.Drawing.Size(314, 32);
-            this.txtTenCV.TabIndex = 2;
-            // 
-            // txtMaCVTim
-            // 
-            this.txtMaCVTim.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.txtMaCVTim.Enabled = false;
-            this.txtMaCVTim.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.8F);
-            this.txtMaCVTim.Location = new System.Drawing.Point(739, 62);
-            this.txtMaCVTim.Name = "txtMaCVTim";
-            this.txtMaCVTim.Size = new System.Drawing.Size(314, 32);
-            this.txtMaCVTim.TabIndex = 8;
-            this.txtMaCVTim.Text = "nhập mã chức vụ tìm...";
-            // 
             // frmChucVu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1056, 525);
+            this.ClientSize = new System.Drawing.Size(792, 427);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "frmChucVu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -263,8 +279,8 @@
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            this.panel3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgvChucVu)).EndInit();
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
